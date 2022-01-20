64a2d530ec67c5d1c7cd2a64b6a1c291003826d8
twopsix <twopsix@gmail.com>
2022-01-20 07:38:27 +0100

    day 1

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <utility>
 
 int main() {
-    std::cout << "Hello World\n";
+    std::fstream input_file("input.txt", std::ios::in);
+    std::string data;
+    input_file >> data;
+    std::map<char, int> dir;
+    dir.insert(std::make_pair('(', 0));
+    dir.insert(std::make_pair(')', 0));
+    for(auto const &c : data) {
+        dir.at(c)++;
+    }
+    std::cout << dir.at('(')-dir.at(')') << '\n';
 }
